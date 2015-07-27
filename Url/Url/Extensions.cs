70ebac22bc97607ac0ab17f088ae234ffedf3f90
Dan McCoy <dan.mccoy@mccoysoftware.uk>
2015-07-27 20:51:29 +0100

    Excluded files added

---
+++ Url/Url/Extensions.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Url
+{
+    public static class Extensions
+    {
+        public static NameValueCollection ParseQuery(this UriBuilder thisUriBuilder)
+        {
+            return HttpUtility.ParseQueryString(thisUriBuilder.Query);
+        }
+
+        public static void SetQuery(this UriBuilder thisUriBuilder, string query)
+        {
+            if (thisUriBuilder != null)
+            {
+                query = (query ?? string.Empty);
+                if (query.StartsWith("?"))
+                    query = query.Remove(0, 1);
+
+                thisUriBuilder.Query = query;
+            }
+        }
+
+        public static void SetQueryParam(this UriBuilder thisUriBuilder, string name, object value)
+        {
+            if (thisUriBuilder != null)
+            {
+                NameValueCollection namedValues = thisUriBuilder.ParseQuery();
+                namedValues.Set(name, value.ToString());
+                thisUriBuilder.SetQuery(namedValues.ToQueryString());
+            }
+        }
+
+        public static void RemoveQueryParam(this UriBuilder thisUriBuilder, string name)
+        {
+            if (thisUriBuilder != null)
+            {
+                NameValueCollection namedValues = thisUriBuilder.ParseQuery();
+                namedValues.Remove(name);
+                thisUriBuilder.SetQuery(namedValues.ToQueryString());
+            }
+        }
+
+        public static string ToQueryString(this NameValueCollection thisCollection)
+        {
+            StringBuilder queryString = new StringBuilder();
+
+            if (thisCollection != null && thisCollection.Count > 0)
+            {
+                foreach (string key in thisCollection)
+                {
+                    queryString.Append(queryString.Length == 0 ? "?" : "&");
+
+                    // Remember that with querystrings, keys can actually be null or empty. Each is slightly different in the querystring:
+                    // Named value:     ?myKey=myValue
+                    // Zero length key: ?=myValue
+                    // Null key:        ?myValue
+                    queryString.Append(key == null ? thisCollection[key] : key + "=" + thisCollection[key]);
+                }
+            }
+
+            return queryString.ToString();
+        }
+    }
+}
---
+++ Url/Url/Url.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Url
+{
+    public class Url : Uri
+    {
+
+        #region properties
+
+        private NameValueCollection _queryParams = null;
+        public NameValueCollection QueryParams
+        {
+            get
+            {
+                if (this._queryParams == null)
+                    this._queryParams = new NameValueCollection();
+                return this._queryParams;
+            }
+        }
+
+        public new string Query
+        {
+            get
+            {
+                return this.QueryParams.ToQueryString();
+            }
+        }
+
+        #endregion
+
+
+        #region constructors
+
+        public Url(string uriString)
+            : base(uriString)
+        {
+            if ((uriString ?? string.Empty).Contains('?'))
+                this._queryParams = ParseQuery(uriString);
+        }
+        public Url(string uriString, UriKind uriKind)
+            : base(uriString, uriKind)
+        {
+            if ((uriString ?? string.Empty).Contains('?'))
+                this._queryParams = ParseQuery(uriString);
+        }
+        public Url(Uri baseUri, string relativeUri)
+            : base(baseUri, relativeUri)
+        {
+            if ((baseUri.ToString() ?? string.Empty).Contains('?'))
+                this._queryParams = ParseQuery(baseUri.ToString());
+        }
+        public Url(Uri baseUri, Uri relativeUri)
+            : base(baseUri, relativeUri)
+        {
+            if ((baseUri.ToString() ?? string.Empty).Contains('?'))
+                this._queryParams = ParseQuery(baseUri.ToString());
+        }
+        
+        #endregion
+
+
+        #region public methods
+
+
+
+        public Url SetQueryParam(string name, object value)
+        {
+            this.QueryParams.Set(name, value.ToString());
+            return this;
+        }
+
+        public Url RemoveQueryParam(string name)
+        {
+            this.QueryParams.Remove(name);
+            return this;
+        }
+
+        public override string ToString()
+        {
+            string strBaseUri = base.ToString();
+            if (!string.IsNullOrEmpty(base.ToString()) && base.ToString().Contains('?'))
+                strBaseUri = strBaseUri.Substring(0, strBaseUri.IndexOf('?'));
+
+            return string.Concat(strBaseUri, this.Query);
+        }
+
+        #endregion
+
+
+        #region public static methods
+
+        public static NameValueCollection ParseQuery(string strQuery)
+        {
+            NameValueCollection queryParams = null;
+            if (string.IsNullOrEmpty(strQuery))
+                queryParams = new NameValueCollection();
+            else
+            {
+                if (strQuery.Contains('?'))
+                    strQuery = strQuery.Substring(strQuery.IndexOf('?'));
+                queryParams = HttpUtility.ParseQueryString(strQuery);
+            }
+            return queryParams;
+        }
+
+        #endregion
+
+    }
+}
