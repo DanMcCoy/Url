42b63af76f42c9db52270ed55b1d57b732215281
Dan McCoy <dan.mccoy@mccoysoftware.uk>
2015-07-28 20:29:14 +0100

    Tests for Url added

--- Url/UnitTests/UrlTests.cs
+++ Url/UnitTests/UrlTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using UrlUtilities;
+using System.Collections.Specialized;
 
 namespace UnitTests
 {
@@ -15,6 +16,25 @@ namespace UnitTests
             url.SetQueryParam("FirstName", "Arthur");
             url.SetQueryParam("Surname", "Dent");
             Assert.AreEqual("http://mccoysoftware.uk/?Title=Mr&FirstName=Arthur&Surname=Dent", url.ToString());
+        }
+
+        [TestMethod]
+        public void RemoveQueryParamTest()
+        {
+            Url url = new Url("http://mccoysoftware.uk/?Title=Mr&FirstName=Arthur&Surname=Dent");
+            Assert.AreEqual("http://mccoysoftware.uk/?Title=Mr&FirstName=Arthur&Surname=Dent", url.ToString());
+
+            url.RemoveQueryParam("FirstName");
+            Assert.AreEqual("http://mccoysoftware.uk/?Title=Mr&Surname=Dent", url.ToString());
+        }
+
+        [TestMethod]
+        public void ParseQueryTest()
+        {
+            NameValueCollection nvc = Url.ParseQuery("?Title=Mr&FirstName=Arthur&Surname=Dent");
+            Assert.AreEqual("Mr", nvc["Title"]);
+            Assert.AreEqual("Arthur", nvc["FirstName"]);
+            Assert.AreEqual("Dent", nvc["Surname"]);
         }
     }
 }
--- Url/Url/Url.cs
+++ Url/Url/Url.cs
@@ -38,24 +38,50 @@ namespace UrlUtilities
 
         #region constructors
 
+        /// <summary>
+        /// Initializes a new instance of the UrlUtilities.Url class with the specified URL.
+        /// </summary>
+        /// <param name="uriString">A URL</param>
         public Url(string uriString)
             : base(uriString)
         {
             if ((uriString ?? string.Empty).Contains('?'))
                 this._queryParams = ParseQuery(uriString);
         }
+
+        /// <summary>
+        /// Initializes a new instance of the UrlUtilities.Url class with the specified URL.
+        /// This constructor allows you to specify if the URI string is a relative URI,
+        /// absolute URI, or is indeterminate.
+        /// </summary>
+        /// <param name="uriString">A string that identifies the resource to be represented by the System.Uri instance.</param>
+        /// <param name="uriKind">Specifies whether the URI string is a relative URI, absolute URI, or is indeterminate.</param>
         public Url(string uriString, UriKind uriKind)
             : base(uriString, uriKind)
         {
             if ((uriString ?? string.Empty).Contains('?'))
                 this._queryParams = ParseQuery(uriString);
         }
+
+        /// <summary>
+        /// Initializes a new instance of the UrlUtilities.Url class based on the 
+        /// specified base URI and relative URI string.
+        /// </summary>
+        /// <param name="baseUri">The base URL</param>
+        /// <param name="relativeUri">The relative URL to add to the base URL</param>
         public Url(Uri baseUri, string relativeUri)
             : base(baseUri, relativeUri)
         {
             if ((baseUri.ToString() ?? string.Empty).Contains('?'))
                 this._queryParams = ParseQuery(baseUri.ToString());
         }
+
+        /// <summary>
+        /// Initializes a new instance of the UrlUtilities.Url class based on the combination
+        /// of a specified base System.Uri instance and a relative System.Uri instance.
+        /// </summary>
+        /// <param name="baseUri">An absolute System.Uri that is the base for the new System.Uri instance.</param>
+        /// <param name="relativeUri">A relative System.Uri instance that is combined with baseUri.</param>
         public Url(Uri baseUri, Uri relativeUri)
             : base(baseUri, relativeUri)
         {
@@ -66,20 +92,38 @@ namespace UrlUtilities
         #endregion
 
 
-        #region public methods
+        #region public methodsm
 
+        /// <summary>
+        /// Adds or updates a querystring parameter key and value.
+        /// </summary>
+        /// <param name="name">The querystring key</param>
+        /// <param name="value">The querystring value to set</param>
+        /// <returns>This Url object (for the purpose of method chaining)</returns>
         public Url SetQueryParam(string name, object value)
         {
             this.QueryParams.Set(name, value.ToString());
             return this;
         }
 
+        /// <summary>
+        /// Removes the named querystring key and it's value
+        /// </summary>
+        /// <param name="name">The name of the querystring parameter to remove</param>
+        /// <returns>This Url object (for the purpose of method chaining)</returns>
         public Url RemoveQueryParam(string name)
         {
             this.QueryParams.Remove(name);
             return this;
         }
 
+        /// <summary>
+        /// Gets a canonical string representation for the specified System.Uri instance
+        /// </summary>
+        /// <returns>
+        /// A System.String instance that contains the unescaped canonical representation
+        /// of the System.Uri instance. All characters are unescaped except #, ?, and %.
+        /// </returns>
         public override string ToString()
         {
             string strBaseUri = base.ToString();
@@ -94,6 +138,11 @@ namespace UrlUtilities
 
         #region public static methods
 
+        /// <summary>
+        /// Parses the given querystring into a NameValueCollection.
+        /// </summary>
+        /// <param name="strQuery">The querystring to parse</param>
+        /// <returns>A NameValueCollection containing the keys and values from the querystring</returns>
         public static NameValueCollection ParseQuery(string strQuery)
         {
             NameValueCollection queryParams = null;
